a141cf97d8910a6f1878a1ed127696baad8e71ec
stang464 <84437978+wild325@users.noreply.github.com>
2021-05-20 15:59:25 +0700

    main

--- page/employee.php
+++ page/employee.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css">
     <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+
 </head>
 
 <body>
@@ -16,17 +17,16 @@
     <?php include('../component/nevbar.php') ?>
     <?php include('../component/sidebar.php') ?>
 
+    
     <div class='main'>
         <div class="container">
             <h5>Employees</h5>
             <div class="row">
-                <div class="col-11">
+                <div class="col-12">
                     <div class="card">
                         <div class="col-auto mt-2">
                             <div class="card-title ">พนักงานทั้งหมด</div>
                         </div>
-
-
                         <div class="col-auto ">
                             <div class="table-responsive"></div>
                             <table id='table' class='table table-striped table-bordered' style="width:100%">
@@ -77,8 +77,11 @@
                 </div>
 
             </div>
-
         </div>
+
+
+
+    </div>
 
 
     </div>
--- page/index.php
+++ page/index.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="../css/bootstrap/bootstrap.css" rel="stylesheet">
-
     <link href="../css/index.css" rel="stylesheet">
 </head>
 
