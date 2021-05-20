c0657e65087dedc71372f1e9112585b2543c2648
stang464 <84437978+wild325@users.noreply.github.com>
2021-05-20 13:29:16 +0700

    main

--- page/employee.php
+++ page/employee.php
@@ -1,23 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
 
     <?php include('../component/nevbar.php') ?>
     <?php include('../component/sidebar.php') ?>
 
-    <main class='container'>
-        <h5>Employees</h5>
-        <div class="row">
-            
+    <div class='main'>
+        <span>
+            <h5>Employees</h5>
+        </span>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-11">
+                    <div class="card">
+                        
+                    </div>
+                </div>
+            </div>
         </div>
-    
-    </main>
-    
+
+    </div>
+
 </body>
+
 </html>
