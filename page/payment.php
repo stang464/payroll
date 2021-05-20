be0769b85d5903b93cce5aa3e31338158f01f7c6
stang464 <84437978+wild325@users.noreply.github.com>
2021-05-20 15:25:45 +0700

    Create payment.php

---
+++ page/payment.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    include('../component/nevbar.php');
+    include('../component/sidebar.php');
+    ?>
+
+    <div class="main">
+        <div class='ml-1 mt-5 '>
+            <div class="card">
+                <div class="col-12">
+                    <tr></tr>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
