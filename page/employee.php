a5540e07925e4e3f8b38b4751d76e5079d3cab0c
nuttapas <68525325+nuttapas@users.noreply.github.com>
2021-05-20 13:41:31 +0700

    main

---
+++ page/employee.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php include('../component/nevbar.php') ?>
+    <?php include('../component/sidebar.php') ?>
+
+    <main class='container'>
+        <h5>Employees</h5>
+        <div class="row">
+            
+        </div>
+    
+    </main>
+    
+</body>
+</html>
